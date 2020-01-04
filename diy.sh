f8e623167401f82116dec6c2f5da727ab234f513
syfirefox <57096295+syfirefox@users.noreply.github.com>
2020-01-04 22:55:31 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# git clone https://github.com/vernesong/OpenClash.git && mv OpenClash/luci-app-openclash openwrt/package/luci-app-openclash
+# git clone https://github.com/Mrbai98/luci-theme-atmaterial.git openwrt/package/luci-theme-atmaterial
